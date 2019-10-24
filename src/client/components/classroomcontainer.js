723d09d35c5d13e145bf94a9048eb75f8099e195
nicoraven <nicoraventbh@gmail.com>
2019-10-24 15:37:12 +0800

    added sprite sheet for logo

--- src/client/components/classroomcontainer.js
+++ src/client/components/classroomcontainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 
 import './classroomcontainer.css';
@@ -9,7 +10,16 @@ export default class ClassroomsContainer extends Component {
 
   render() {
     const { classrooms } = this.props;
-    const classroomsList = classrooms.map(room => <Classroom room={room} />);
+    console.log(classrooms);
+    const classroomsList = classrooms.length > 0 ? (
+      classrooms.map(room => <Classroom room={room} />)
+    ) : (
+      <div className="classroom">
+        <div className="classroom-content">
+          <h4>Add a classroom here.</h4>
+        </div>
+      </div>
+    );
 
     return (
       <div className="classrooms-container">
@@ -22,13 +32,22 @@ export default class ClassroomsContainer extends Component {
   }
 }
 
-const Classroom = (room) => {
+const Classroom = ({ room }) => {
+  console.log(room);
   const sessionName = room.session ? room.session.name : 'Vacant';
   return (
-    <div className="classroom" key={room.id}>
+    <div key={room.id} className="classroom">
       <div className="classroom-content">
-        <h4>{room.name}</h4>
-        <p>Current Session: {sessionName}</p>
+        <div className="classroom-details">
+          <h3>{room.name}</h3>
+          <p>{sessionName}</p>
+        </div>
+        <div className="classroom-icons">
+          <div className="icon" id="icon-view" alt="view" />
+          <div className="icon" id="icon-set" alt="set" />
+          <div className="icon" id="icon-clear" alt="clear" />
+          <div className="icon" id="icon-settings" alt="settings" />
+        </div>
       </div>
     </div>
   );
